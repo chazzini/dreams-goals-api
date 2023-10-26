8c8c0f8f81e346a40dd29b5536952cd7e39b8e59
Chinedu Chaz <ccognate@gmail.com>
2023-10-26 10:51:25 +0100

    Basic Model and Controller Setup
    Creation of  Confession model, Goal model, Why model with respective
    controllers.
    Setting the api version to using route version starting from
    version 1 (api/v1)

---
+++ database/seeders/WhySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class WhySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
